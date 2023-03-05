ef87dcd94136ec0e710412e55c34b1662c878250
Vince van Schagen <vincevanschagen@gmail.com>
2023-03-05 19:47:20 +0100

    check commit

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 const express = require('express')
-const ejs = require('ejs')
+// const ejs = require('ejs')
 const path = require('path')
 
 const app = express()
 
 const port = 4000
 
+
+
 app.use(express.static('static'))
+app.set("view engine", "ejs")
+app.set("views", "view")
+
+var userlist = [
+  {
+    id: 'Vincius70',
+    name: 'Vince',
+    description: 'retired sage main'
+  },
+  {
+    id: 'ellie',
+    name: 'iridescent',
+    description: 'instalock jett'
+  }
+  ]
+  
+  app.get('/lijst', function users(req, res) {
+    res.render('list.ejs', {username: userlist[0].name})
+  })
+
+
+
+  
+
 
 app.use(function(req, res, next) {
-  res.status(404).send ("<h1>Error 404</h1> <p>self destruct mode in 10 seconds</p>");
+  res.status(404).send ("<h1>Error 404</h1> <p>self destruct mode in 10 seconds</p> ");
 });
-
-app.get('/hallo', (req, res) => {
-  res.send('<img src="/images/hont_eent.jpg" width="250">Hello World!')
-})
-
-app.get('/home/:user/', (req, res) => {
-  console.log(`Input from ${req.params.user}`)
-  res.send('<img src="/img/hont_eent.jpg" width="250"><h1>Hello ' + req.params.user + '</h1>')
-})
-
-app.get('/', function(request, response) {
-  response.render('index')
-})
-
-app.set('views', path.join(__dirname, 'views'))
-app.set('view egine', 'ejs')
 
 app.listen(port, () => {
     console.log(`Mic check on port ` + port)

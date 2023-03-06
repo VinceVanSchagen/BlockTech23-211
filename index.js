db3cc3c594c7c62c8168cbb89a93afed9ea73c68
Vince van Schagen <vincevanschagen@gmail.com>
2023-03-06 14:58:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express')
-// const ejs = require('ejs')
 const path = require('path')
-
 const app = express()
-
 const port = 4000
 
 
@@ -11,28 +8,6 @@ const port = 4000
 app.use(express.static('static'))
 app.set("view engine", "ejs")
 app.set("views", "view")
-
-var userlist = [
-  {
-    id: 'Vincius70',
-    name: 'Vince',
-    description: 'retired sage main'
-  },
-  {
-    id: 'ellie',
-    name: 'iridescent',
-    description: 'instalock jett'
-  }
-  ]
-  
-  app.get('/lijst', function users(req, res) {
-    res.render('list.ejs', {username: userlist[0].name})
-  })
-
-
-
-  
-
 
 app.use(function(req, res, next) {
   res.status(404).send ("<h1>Error 404</h1> <p>self destruct mode in 10 seconds</p> ");

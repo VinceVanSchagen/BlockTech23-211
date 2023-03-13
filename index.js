dbe3e5b9ad0d0ed24d3a3878ac298057add42c3c
Vince van Schagen <vincevanschagen@gmail.com>
2023-03-13 10:30:41 +0100

    overbodige files verwijderd en html aangepast

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+// require('dotenv').config()
+const port = 4000
+
 const express = require('express')
 const ejs = require('ejs')
 const path = require('path')
+// const uri = process.env.MONGODB_URI;
+// const uri = 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PASS + process.env.DB_NAME + '/' + process.env.DB_HOST + '?retryWrites=true&w=majority';
+
 const { MongoClient } = require("mongodb");
-const uri ="mongodb+srv://vincevanschagen:VInciusMONGODB.70@cluster0.byrbpri.mongodb.net/?retryWrites=true&w=majority";
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = "mongodb+srv://vincevanschagen:VInciusMONGODB70@cluster0.byrbpri.mongodb.net/?retryWrites=true&w=majority"
+
 const client = new MongoClient(uri);
 
-const app = express()
 
-const port = 4000
+
+const app = express()
 
 async function run() {
   try {
@@ -16,7 +24,7 @@ async function run() {
     const db = client.db("sample_guides");
     const coll = db.collection("planets");
     // find code goes here
-    const cursor = coll.find({ hasRings: false, mainAtomsphere: "n" });
+    const cursor = coll.find();
     // iterate code goes here
     await cursor.forEach(console.log);
   } finally {
@@ -25,6 +33,7 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
 
 app.use(express.static('static'))
 app.set("view engine", "ejs")
